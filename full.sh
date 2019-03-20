aa422ec61557a3b7a83b3082d2b4bf43adc0d8d7
mstukalo <mykola.stukalo@gmail.com>
2019-03-20 20:39:11 +0200

    Оновив скрипт для генерації карти Словаччини

--- full.sh
+++ full.sh
@@ -9,19 +9,19 @@ rm -r mkgmap-stranger/_intermediate/*
 rm -r mkgmap-stranger/_split/*
 rm UA.osm.pbf
 
-wget -N http://data.gis-lab.info/osm_dump/dump/latest/UA.osm.pbf
+wget -N https://download.geofabrik.de/europe/slovakia-latest.osm.pbf
 
 cd mkgmap-stranger
 
 echo Splitting
-java -Xmx1G -jar ../executable/splitter/splitter.jar ../UA.osm.pbf --output-dir=_split
+java -Xmx1G -jar ../executable/splitter/splitter.jar ../slovakia-latest.osm.pbf --output-dir=_split
 
 echo Creating -uk map with mkgmap
 java -Xmx1000M -jar ../executable/mkgmap/mkgmap.jar --style-file=stranger-uk/stranger --family-id=45 --output-dir=_intermediate --read-config=ukrstranger.cfg stranger-uk/stranger.txt _split/template.args
 
 echo Copying result files
 today=$(date +%Y-%m-%d)
-cp _intermediate/gmapsupp.img ../results/OSM\ Ukraine\ $today.img
+cp _intermediate/gmapsupp.img ../results/OSM\ Slovakia\ $today.img
 cp _intermediate/stranger.typ ../results/stranger.typ
 
 cd ..
@@ -29,4 +29,4 @@ cd ..
 # cleanup
 rm -r mkgmap-stranger/_intermediate/*
 rm -r mkgmap-stranger/_split/*
-rm UA.osm.pbf
+rm slovakia-latest.osm.pbf
